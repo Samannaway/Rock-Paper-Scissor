15d6a029fca1bb850aa809a5f819b80548867a42
Samannaway Ghosh <85423940+Samannaway@users.noreply.github.com>
2022-02-07 21:25:21 +0530

    update v2

--- game/script.js
+++ game/script.js
@@ -1,22 +1,25 @@
-alert("press next round after each move")
+alert("press next round after each move, You will get 3 seconds for each move and the game is of 1 minute. enjoy!!")
 
 
 // initialising variables
 let userClick = document.querySelectorAll(".hand");
 let nextRoundButton = document.querySelector(".next");
-let endGameButton = document.querySelector(".end")
 let playerScoreArea = document.querySelector(".playerScore")
 let computerScoreArea = document.querySelector(".computerScore")
 let computerOutputArea = document.querySelector(".computerOutput")
-let move = [{src : "images/paper.png",move : "paper"},
-            {src : "images/stone.png",move : "stone"},
-            {src : "images/Scissor.png",move : "scissor"}
+let coolDownTimeArea = document.querySelector(".moveCoolDown")
+let gameTimeArea = document.querySelector(".gameTime")
+let move = [
+    {src : "images/paper.png",move : "paper"},
+    {src : "images/stone.png",move : "stone"},
+    {src : "images/Scissor.png",move : "scissor"}
 
-        ]
+]
+let quitGameButton = document.querySelector(".quit")
 let winnerDeclarationArea = document.querySelector(".whoWon");
 var computerInput = []
 var userInput = [];
-let playOrNot = 0;
+let playOrNot = true;
 let compScore = 0;
 let userScore = 0;
 
@@ -31,18 +34,38 @@ function computerPlay(params) {
     console.log(computerInput);
 }
 
+// checking if player is playing or not?
+let playTime = 0;
+
+
+let playtimeCheck = setInterval(function(){
+    playTime++
+    console.log(playTime);
+
+    coolDownTimeArea.innerHTML = "cooldown : " + playTime + " / 5 seconds"  
+
+    if(playTime >= 5 ){
+        endGame()
+        clearInterval(playtimeCheck)
+        coolDownTimeArea.innerHTML = "cooldown time ended"
+
+        gameTimeArea.classList.add("hide")
+    }
+},1000);
+
 
 // checking the input by the user
 for (let i = 0; i < userClick.length; i++) {
     userClick[i].addEventListener("click",function userPlay() {
-        
-        if(playOrNot === 0){
+
+        if(playOrNot === true){
+
+            playTime = 0;
 
             userInput.push(this.classList[1]);
-    
-            
+
             console.log(userInput[0]);
-            playOrNot++
+            playOrNot = false
     
             console.log(playOrNot);
             userClick[0].classList.add("disabled")
@@ -57,21 +80,19 @@ for (let i = 0; i < userClick.length; i++) {
 
 }
 
-// the play function{
-
-// checking if a round is completed
-
 
 // going for next round
 nextRoundButton.addEventListener("click",function () {
     computerOutputArea.src = "";
     userInput = []
     computerInput = []
-    playOrNot = 0;
+    playOrNot = true;
     console.log(userInput);
     userClick[0].classList.remove("disabled")
     userClick[1].classList.remove("disabled")
     userClick[2].classList.remove("disabled")
+
+    playTime = 0;
 });
 
 
@@ -129,7 +150,8 @@ function checkScore(){
 
 }
 // telling results when the game ends - 
-endGameButton.addEventListener("click",function (params) {
+function endGame(params) {
+
     if(userScore==compScore){
         winnerDeclarationArea.innerHTML = "The game draws"
     }else if(compScore<userScore){
@@ -137,8 +159,52 @@ endGameButton.addEventListener("click",function (params) {
     }else{
         winnerDeclarationArea.innerHTML = "computer won the game"
     }
-})
 
-function whenGameEnds() {
-    
+
+    playOrNot = 1;
+    nextRoundButton.classList.add("hide")
+    nextRoundButton.classList.remove("Button")
+    quitGameButton.classList.add("center")
+    quitGameButton.classList.remove("Button")
 }
+    
+
+
+// putting game time - 
+var seconds = 0
+
+
+function timer() {
+    let timerInterval = setInterval(()=>{
+        if(seconds<60){
+
+            seconds++
+            console.log(seconds);
+            gameTimeArea.innerHTML =  seconds + " / 60 seconds left"
+
+        }else if(seconds >= 60){
+
+            gameTimeArea.innerHTML = "time ended"
+
+            console.log("time ended");
+            playOrNot = 1;
+            nextRoundButton.classList.add("hide")
+            userClick[0].classList.add("disabled")
+            userClick[1].classList.add("disabled")
+            userClick[2].classList.add("disabled")
+            computerOutputArea.classList.add("disabled")
+            quitGameButton.classList.add("center")
+            quitGameButton.classList.remove("Button")
+            endGame()
+        }
+    },1000)
+    
+
+    if(playTime >= 5){
+        clearInterval(timerInterval)
+    }
+}
+
+
+
+timer()
